c6d73d8cf08f7e88a2f4f2ebc241efa9f324dd5c
derp000 <84947701+derp000@users.noreply.github.com>
2024-03-03 15:52:21 -0500

    Finish homepage mostly

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -23,8 +23,8 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="relative bg-emerald-700 h-[45rem] py-12">
-          <div className="absolute top-[-5rem] left-0 right-0 z-10 bg-white shadow-2xl shadow-green-900 rounded-lg p-36 text-2xl w-[72rem] m-auto">
+        <div className="relative bg-emerald-950 h-[45rem] py-12">
+          <div className="absolute top-[-5rem] left-0 right-0 z-10 bg-white shadow-2xl shadow-zinc-700 rounded-lg p-36 text-2xl w-[72rem] m-auto">
             <p>
               FIRST is an international, not-for-profit organization that was
               founded in 1989 in Manchester, NH. FIRST had goals to inspire
@@ -42,41 +42,36 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="p-24 bg-teal-950">
-          <div>
-            <div className="flex flex-col gap-2 text-white max-w-[72rem] mx-auto mb-12">
-              <h1 className="text-center text-5xl font-bold">Our Impact</h1>
-              <p className="text-center text-2xl leading-tight">
-                Parallel Universe is deeply committed to STEM outreach and
-                education. Team 1257 is a proud co-host of RVSTS, and its
-                members often volunteer at FLL events.
-              </p>
+        <div className="p-24 bg-emerald-700">
+          <div className="flex flex-row justify-between text-center text-xl font-extrabold gap-2 text-white m-auto max-w-[72rem] mb-12">
+            <div>
+              <h1 className="text-[15dvh]">125</h1>
+              <p className="font-semibold">Number of bananas</p>
             </div>
-            <div className="flex flex-col items-center">
-              <img
-                src="https://placehold.co/640x360"
-                alt="placeholder"
-                className="mb-12"
-              />
-              <Button
-                text="Learn more"
-                style="bg-green-700 hover:bg-green-500 text-white blob-green"
-                to="/info"
-              />
+            <div>
+              <h1 className="text-[15dvh]">125</h1>
+              <p className="font-semibold">Number of apples</p>
+            </div>
+            <div>
+              <h1 className="text-[15dvh]">125</h1>
+              <p className="font-semibold">Number of cherries</p>
             </div>
           </div>
         </div>
-        <div className="bottom-arrow bg-gradient-to-r p-16 from-purple-600 from-50% to-blue-400 ">
+        <div className="p-16 bg-white">
           <div>
-            <div className="flex flex-col gap-2 text-white max-w-[72rem] mx-auto mb-12">
-              <h1 className="text-center text-5xl font-bold">Get Involved</h1>
-              <p className="text-center text-2xl leading-tight">
-                Running an award-winning robotics team takes significant
-                personal and financial contribution. We are always seeking new
-                mentors and sponsorships. We appreciate all the help we can get!
+            <div className="flex flex-col gap-2 text-green-950 items-center mb-12">
+              <h1 className="text-5xl font-bold">Get Started</h1>
+              <p className="text-2xl leading-tight mb-4">
+                Here are some resources to go green. Please do so to help the
+                environment.
               </p>
+              <Button
+                text="Start now"
+                style="bg-zinc-800 text-white"
+                to="/info"
+              />
             </div>
-            <Button text="Learn more" style="bg-white text-black" to="/info" />
           </div>
         </div>
       </div>
